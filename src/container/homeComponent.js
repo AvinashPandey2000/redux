81d93bdb6c52afc15e9d22f61b99d80a54f6b1de
unknown <AvinashPandey2000>
2023-03-24 20:59:00 -0700

    initial commit

---
+++ src/container/homeComponent.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from '../components/home'
+
+const homeComponent = () => {
+  return (
+    <View>
+      <Home/>
+    </View>
+  )
+}
+
+export default homeComponent
